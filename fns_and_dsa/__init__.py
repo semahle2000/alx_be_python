7b26841d1d9b93d3c65d48ccd2f55869fd1b2fb9
Asemahle botha <asemahlebothaasez@gmail.com>
2025-01-07 13:39:37 -0800

    Add arithmetic_operations.py file

---
+++ fns_and_dsa/__init__.py
@@ -0,0 +1,8 @@
+# fns_and_dsa/__init__.py
+
+# You can import the perform_operation function for easier access
+from .arithmetic_operations import perform_operation
+
+# You can also define package-level variables or functions if needed
+__all__ = ["perform_operation"]
+
---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+# fns_and_dsa/arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return "Cannot divide by zero"
+            else:
+                return num1 / num2
+        case _:
+            return "Invalid operation"
+
